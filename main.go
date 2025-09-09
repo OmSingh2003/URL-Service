33f1487bc859ba86bea93360a9bd42eeb0c173e7
Om Singh <omsingh.ailearn@gmail.com>
2025-09-10 03:19:37 +0530

    connected to mongoDB

--- main.go
+++ main.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	//"net/http"
+	"context"
+	"fmt"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	mongo_URI := os.Getenv("MONGO_URI")
+	opts := options.Client().ApplyURI(mongo_URI).SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("connected to MongoDB")
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
